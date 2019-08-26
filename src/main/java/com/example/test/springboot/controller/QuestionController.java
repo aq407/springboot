e40c874ba345122bcd76b65eead31522b1c4d9bd
aq407 <929193338@qq.com>
2019-08-26 12:20:47 +0800

    new

--- src/main/java/com/example/test/springboot/controller/QuestionController.java
+++ src/main/java/com/example/test/springboot/controller/QuestionController.java
@@ -1,24 +1,26 @@
 package com.example.test.springboot.controller;
 
-import com.example.test.springboot.dto.CommentDTO;
 import com.example.test.springboot.dto.QuestionDTO;
 import com.example.test.springboot.mapper.CommentMapper;
 import com.example.test.springboot.moder.Comment;
 import com.example.test.springboot.moder.User;
 import com.example.test.springboot.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.System.out;
 
 @Controller
 @RequestMapping("/question")
 public class QuestionController {
     @Autowired
-    private QuestionService  questionService;
+    private QuestionService questionService;
 
     @Autowired
     private CommentMapper commentMapper;
@@ -27,35 +29,35 @@ public class QuestionController {
     public String question(@PathVariable(name = "id") Integer id,
                            Model model) {
         QuestionDTO questionDTO = questionService.getById(id);
-        //model.addAttribute("question",questionDTO);
-
-        List <Comment> comments = commentMapper.getReply();
-
-
-       // List <User> User = commentMapper.getIco(comment.getParentId());
-        //String ico=null;
-        //
-        //System.out.println(ico);
-        model.addAttribute("comment",comments);
-        model.addAttribute("ids",id);
-
+        model.addAttribute("question", questionDTO);
+        List<Comment> comments = commentMapper.getReply();
+        model.addAttribute("comment", comments);
+        model.addAttribute("ids", id);
         return "question";
     }
 
     @PostMapping("/{id}")
     public String comment(@PathVariable(name = "id") Integer id,
-                          @RequestParam (name = "questionId") Integer questionId,
-                          @RequestParam (name = "content") String content,
-                          @RequestParam (name = "questionTitle") String questionTitle,
+                          @RequestParam(name = "questionId") Integer questionId,
+                          @RequestParam(name = "content") String content,
+                          @RequestParam(name = "questionTitle") String questionTitle,
                           Model model,
-                          HttpServletRequest request){
+                          HttpServletRequest request) {
+        //Object user = request.getSession().getAttribute("user");
         String question = "/question/";
         String num = id.toString();
         String html = question + num;
 
-        System.out.println(content);//回复内容 content
-        System.out.println(questionId);//回帖id parent_id
-        User user = (User) request.getSession().getAttribute("user");
+        if (content == "") {
+            //return "forward:/"+"question";
+            return "redirect:/"+"question"+"/"+num;
+        }
+
+        else {
+
+            System.out.println(content);//回复内容 content
+            System.out.println(questionId);//回帖id parent_id
+            User user = (User) request.getSession().getAttribute("user");
 
             //回复人id commentator
             Long time = System.currentTimeMillis();//回复时间 gmt_modified
@@ -66,11 +68,11 @@ public class QuestionController {
             comment.setCommentator(user.getId());
             comment.setQuestionTitle(questionTitle);
             comment.setGmt_create(time);
-            //comment.setIco(user.getIconurl());
+            comment.setIco(user.getIconurl());
             comment.setName(user.getUsername());
             commentMapper.reply(comment);
-            return "redirect:"+html;
+            return "redirect:" + html;
+        }
     }
-
 
 }
